6ff7be4df8dcc9533da1e9c00b4c0bb9359a72bf
Cheese <Cheese@DESKTOP-QLNLMSL>
2024-11-13 23:59:10 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 10;
+        if ( a > b)
+        {
+            Console.WriteLine("a > b");
+        }
+        if (a == b)
+        {
+            Console.WriteLine("a == b");
+
+        }
+        else
+        {
+            Console.WriteLine("a < b");
+        }
+        
+    }
+}
